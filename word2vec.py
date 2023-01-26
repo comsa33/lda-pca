447b1f6a59c441c34b87865e25fca611f414f38d
ruo <ruo@gravylab.co.kr>
2023-01-26 16:24:47 +0900

    add: [AI-138] https://gravytech.atlassian.net/jira/software/projects/AI/boards/2\?selectedIssue\=AI-138

--- word2vec.py
+++ word2vec.py
@@ -3,10 +3,12 @@ from gensim.models import Word2Vec
 from kiwipiepy import Kiwi
 
 import functions as funcs
+import preprocess as prep
 
 
+prep.user_dictionary()
 kiwi = Kiwi()
-# kiwi.load_user_dictionary('user_dictionary.txt')
+kiwi.load_user_dictionary('user_dictionary.txt')
 kiwi.prepare()
 
 
@@ -14,7 +16,7 @@ class Word2VecModel():
 
     def __init__(self):
         '''주요 품사 정의'''
-        self.주요품사 = ["NNG", "NNP", "VV", "VA", "XR"]
+        self.주요품사 = ["NNG", "NNP", "VV", "VA", "XR", "SL"]
         self.용언품사 = ["VV", "VA"]
 
     '''형태소 분석 결과를 읽어서 주요 품사만 수집한 문서 리스트를 돌려준다.'''
@@ -27,25 +29,13 @@ class Word2VecModel():
             문서리스트.append(필터링결과)
         return 문서리스트
 
-    '''주어진 문서 집합으로 문서-어휘 행렬을 만들어 돌려준다.'''
-    def build_doc_term_mat(self, 문서리스트):
-        dictionary = corpora.Dictionary(문서리스트)
-        corpus = [dictionary.doc2bow(문서) for 문서 in 문서리스트]
-        return corpus, dictionary
-
-    '''문서-어휘 행렬을 TF-IDF 문서-단어 행렬로 변환한다.'''
-    def build_corpus_tfidf(self, corpus):
-        tfidf = models.TfidfModel(corpus)
-        corpus_tfidf = tfidf[corpus]
-        return corpus_tfidf
-
     def get_w2v_model(self, df, company_name, year, col):
         df_comp = funcs.get_comp(df, company_name)
         df_comp_ = df_comp[[col, 'year']]
         df_year = df_comp_.query(f'year == {year}')
+        df_year[col] = df_year[col].apply(prep.preprocess_text)
         morph_analysis = lambda x: kiwi.tokenize(x) if type(x) is str else None
         df_year['morpheme'] = df_year[col].apply(morph_analysis)
         doc_list = self.read_documents(df_year, "morpheme")
-        print(f"{year} DATA LENGTH :", len(doc_list))
         model = Word2Vec(doc_list, window=5, min_count=3, workers=4, sg=0)
         return model
